b8acab6677198d3556c3a7d7f8a8c19cfb92db42
viettoan1020 <viettoann102003@gmail.com>
2024-01-09 14:40:50 +0700

    case

---
+++ Store_Manager/src/Inventory.java
@@ -0,0 +1,50 @@
+import java.time.LocalDate;
+
+public class Inventory {
+    private String id;
+    private String name;
+    LocalDate manufacturingDate;
+    int cost;
+
+    public Inventory() {
+    }
+
+    public Inventory(String id, String name, LocalDate manufacturingDate, int cost) {
+        this.id = id;
+        this.name = name;
+        this.manufacturingDate = manufacturingDate;
+        this.cost = cost;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getManufacturingDate() {
+        return manufacturingDate;
+    }
+
+    public void setManufacturingDate(LocalDate manufacturingDate) {
+        this.manufacturingDate = manufacturingDate;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
